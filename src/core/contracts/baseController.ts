ad0cfeeef8b4919a9ae9cb6003b45fb6d064b34a
mohamadreza mosalli <mohamadresaaa@gmail.com>
2023-05-22 16:49:13 +0330

    feat: send response

--- src/core/contracts/baseController.ts
+++ src/core/contracts/baseController.ts
@@ -1,16 +1,25 @@
 import autoBind from 'auto-bind'
+import { Response } from 'express'
 import models from '../../models'
 import { Models } from './models'
 
 export default abstract class BaseController {
   protected models: Models
+
   constructor() {
     autoBind(this)
 
     this.models = { ...models }
   }
 
-  sayMsg() {
-    return 'message'
+  /**
+   * @method sendResponse
+   * @param {response} res
+   * @param {number} status default 200
+   * @param {object} data
+   * @returns response server
+   */
+  sendResponse(res: Response, status = 200, data?: object): Response {
+    return res.status(status).json(data)
   }
 }
