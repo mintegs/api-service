a91e29b69e14e506ffe2dbbf87dbb97a70ddc132
mohamadreza mosalli <mohamadresaaa@gmail.com>
2023-06-11 15:44:45 +0330

    feat: add routes of article

--- src/router/user.ts
+++ src/router/user.ts
@@ -4,10 +4,15 @@ const userRouter: Router = express.Router()
 
 // controller
 import currentUserController from '../controllers/current.user.controller'
+import articleUserController from '../controllers/user/article.user.controller'
 import sessionUserController from '../controllers/user/session.user.controller'
 
 userRouter.get('/', currentUserController.information)
 userRouter.get('/sessions', sessionUserController.findAll)
 userRouter.delete('/sessions/:id', sessionUserController.delete)
+userRouter.get('/articles', articleUserController.findAll)
+userRouter.post('/articles', articleUserController.create)
+userRouter.put('/articles/:id', articleUserController.update)
+userRouter.delete('/articles/:id', articleUserController.delete)
 
 export default userRouter
