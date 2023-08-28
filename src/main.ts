a46d944dd1adf195f0e32443e27481482046f84e
mohamadreza mosalli <mohamadresaaa@gmail.com>
2023-08-28 13:21:54 +0330

    feat: add mongo database

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { createServer } from 'http'
+import { connect, set } from 'mongoose'
 import Core from './core'
 
 // Creating new instance of core
 const core = new Core()
 
 export const start = async () => {
-  const port = process.env.PORT
-  // Running server
-  await createServer(core.getApp()).listen(port, () =>
-    console.log(`server is running on ${port}`)
-  )
+  try {
+    // Setup database and set configurations mongodb
+    set('strictQuery', false)
+    await connect(process.env.DATABASE_URL)
+    console.log('Database connected')
+
+    // Running server
+    const port = process.env.PO
+    await createServer(core.getApp()).listen(port, () =>
+      console.log(`Server is running on ${port}`)
+    )
+  } catch (error) {
+    throw error
+  }
 }
