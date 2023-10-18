97622ad643e1791f9479e95e02582051b27e33d6
mohamadreza mosalli <mohamadresaaa@gmail.com>
2023-10-18 15:04:24 +0330

    changeset: update article repository

--- src/core/repositories/article.repository.ts
+++ src/core/repositories/article.repository.ts
@@ -25,7 +25,7 @@ export class ArticleRepository {
         path: 'category',
         select: 'title -_id',
       })
-      .select('title image content -_id')
+      .select('title status createdAt updatedAt -_id')
       .sort('-createdAt')
       .lean()
   }
